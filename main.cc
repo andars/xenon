e96b3cadfe5d17bef9957506b3eacf9bfba22f26
andars <afoote97@gmail.com>
2016-03-12 17:33:58 -0700

    initial

---
+++ main.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+
+#include "vec3.h"
+#include "ray.h"
+
+bool hit_sphere(const vec3& center, float radius, const ray& r) {
+    vec3 oc = r.origin() - center;
+    float a = dot(r.direction(), r.direction());
+    float b = 2.0*dot(r.direction(), oc);
+    float c = dot(oc,oc) - radius * radius;
+    float discriminant = b*b - 4*a*c;
+    return (discriminant > 0);
+}
+
+vec3 ray_color(const ray& r) {
+    vec3 unit_dir = unit_vector(r.direction());
+    float t = 0.5*(unit_dir.y() + 1.0);
+    return (1.0-t)*vec3(1.0,1.0,1.0) + t*vec3(0.5,0.7,1.0);
+}
+
+int main() {
+    int nx = 200;
+    int ny = 100;
+    std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+    vec3 lower_left_corner(-2.0, -1.0, -1.0);
+    vec3 horizontal(4.0, 0.0, 0.0);
+    vec3 vertical(0.0, 2.0, 0.0);
+    vec3 origin(0.0, 0.0, 0.0);
+
+    for (int j = ny-1; j >= 0; j--) {
+        for (int i = 0; i < nx; i++) {
+            float u = float(i) / float(nx);
+            float v = float(j) / float(ny);
+            ray r(origin, lower_left_corner + u*horizontal + v*vertical);
+            vec3 color = ray_color(r);
+            int ir = int(255.99*color[0]);
+            int ig = int(255.99*color[1]);
+            int ib = int(255.99*color[2]);
+
+            std::cout << ir << " " << ig << " " << ib << "\n";
+        }
+    }
+}
