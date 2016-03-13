677af5865901b636ee3a1567ccc5322853924c85
andars <afoote97@gmail.com>
2016-03-12 17:37:54 -0700

    Change color if ray hits sphere, change background

--- main.cc
+++ main.cc
@@ -13,9 +13,12 @@ bool hit_sphere(const vec3& center, float radius, const ray& r) {
 }
 
 vec3 ray_color(const ray& r) {
+    if (hit_sphere(vec3(0,0,-1), 0.5, r)) {
+        return vec3(1, 0, 0);
+    }
     vec3 unit_dir = unit_vector(r.direction());
     float t = 0.5*(unit_dir.y() + 1.0);
-    return (1.0-t)*vec3(1.0,1.0,1.0) + t*vec3(0.5,0.7,1.0);
+    return (1.0-t)*vec3(0.0, 0.0, 0.0) + t*vec3(0.5, 0.7, 1.0);
 }
 
 int main() {
