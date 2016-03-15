08bb3a79bdef945294199788baa157a8858befc2
andars <afoote97@gmail.com>
2016-03-15 13:23:34 -0600

    add single threaded version of rand_float
    for convenience use in scene setup

--- utils.cc
+++ utils.cc
@@ -3,9 +3,18 @@
 #include "vec3.h"
 #include "context.h"
 
+// for use in setting up scenes, etc
+float rand_float() {
+    static std::default_random_engine gen;
+    static std::uniform_real_distribution<float> dist(0,1);
+    return dist(gen); 
+}
+
+// for multithreaded use, no contention
 float rand_float(const context& ctx) {
     return ctx.rand_real(ctx.gen);
 }
+
 vec3 rand_in_unit_sphere(const context& ctx) {
     vec3 p; 
     do {
--- utils.h
+++ utils.h
@@ -1,6 +1,7 @@
 #ifndef UTILS_H_
 #define UTILS_H_
 
+float rand_float();
 float rand_float(const context& ctx);
 vec3 rand_in_unit_sphere(const context& ctx);
 vec3 reflect(const vec3& v, const vec3& n);
