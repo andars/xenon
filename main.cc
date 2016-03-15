f2c62892a3635c342912ba294ca8a6b33753dd5f
andars <afoote97@gmail.com>
2016-03-15 10:35:54 -0600

    create render function & set up multithreading
    not quite ready for multithreading yet because of contention
    on random generator

--- main.cc
+++ main.cc
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <cmath>
+#include <stdint.h>
+#include <thread>
 
 #include "vec3.h"
 #include "utils.h"
@@ -15,7 +17,9 @@
 
 using std::sqrt;
 
-vec3 ray_color(const ray& r, entity* world, int depth) {
+const int thread_count = 1;
+
+vec3 ray_color(const ray& r, const entity* world, int depth) {
     hit_record rec;
     if (world->hit(r, 0.01f, MAXFLOAT, rec)) {
         ray scattered;
@@ -37,7 +41,34 @@ vec3 ray_color(const ray& r, entity* world, int depth) {
     //return (1.0f-t)*vec3(0.0, 0.0, 0.0) + t*vec3(0.1, 0.4, 0.90);
 }
 
+void render(int nx, int ny, int ns, const entity* world, const camera& cam, uint8_t* pixels) {
+    for (int j = 0; j < ny; j++) {
+        for (int i = 0; i < nx; i++) {
+            vec3 color(0,0,0);
+
+            for (int s = 0; s < ns; s++) {
+                float u = float(i + rand_float()) / float(nx);
+                float v = float(j + rand_float()) / float(ny);
+                ray r = cam.get_ray(u, v);
+                color += ray_color(r, world, 0);
+            }
+
+            color /= float(ns);
+            color = vec3(sqrt(color[0]), sqrt(color[1]), sqrt(color[2]));
+
+            int ir = int(fmin(254.99*color[0],255));
+            int ig = int(fmin(254.99*color[1],255));
+            int ib = int(fmin(254.99*color[2],255));
+
+            pixels[3*nx*j + 3*i + 0] = ir;
+            pixels[3*nx*j + 3*i + 1] = ig;
+            pixels[3*nx*j + 3*i + 2] = ib;
+        }
+    }
+}
+
 int main() {
+    srand(time(0));
     int nx = 300;//6000;
     int ny = 200;//4500;
     int ns = 800;
@@ -75,31 +106,37 @@ int main() {
     entity *world = new entity_list(list, entity_count);
     //(300,0,600), (0,0,100)
     //camera cam(vec3(300.0,0.0,600.0), vec3(0,0,200), vec3(0,1,0), 50, float(nx)/float(ny));
-    camera cam(vec3(0.0,0.0,1.0), vec3(0,0,0), vec3(0,1,0), 50, float(nx)/float(ny));
+    camera cam(vec3(0,0,1), vec3(0,0,0), vec3(0,1,0), 50, float(nx)/float(ny));
+
+    uint8_t** images = new uint8_t*[thread_count];
+    for (int i = 0; i<thread_count; i++) {
+        images[i] = new uint8_t[nx*ny*3];
+    }
+
+    std::thread threads[thread_count];
+    for (int i = 0; i<thread_count; i++) {
+        threads[i] = std::thread(render, nx, ny, ns/thread_count, world, cam, images[i]);
+    }
+
+    for (int i = 0; i<thread_count; i++) {
+        threads[i].join();
+    }
+
+    uint8_t* final = new uint8_t[nx * ny * 3];
+
+    for (int i = 0; i < ny*nx*3; i++) {
+            float value = 0;
+            for (int t = 0; t < thread_count; t++) {
+                value += images[t][i];
+            }
+            final[i] = int(fmin(value/thread_count, 255));
+    }
 
     for (int j = ny-1; j >= 0; j--) {
         for (int i = 0; i < nx; i++) {
-            vec3 color(0,0,0);
-            for (int s = 0; s < ns; s++) {
-                float u = float(i + rand_float()) / float(nx);
-                float v = float(j + rand_float()) / float(ny);
-                ray r = cam.get_ray(u, v);
-                color += ray_color(r, world, 0);
-            }
-            color /= float(ns);
-            color = vec3(sqrt(color[0]), sqrt(color[1]), sqrt(color[2]));
-
-            int ir = int(255.99*color[0]);
-            int ig = int(255.99*color[1]);
-            int ib = int(255.99*color[2]);
-
-            /*
-            image[4*nx*(ny-j) + 4*i + 0] = ir;
-            image[4*nx*(ny-j) + 4*i + 1] = ig;
-            image[4*nx*(ny-j) + 4*i + 2] = ib;
-            image[4*nx*(ny-j) + 4*i + 3] = 255;
-            */
-
+            int ir = final[3*nx*j + 3*i + 0];
+            int ig = final[3*nx*j + 3*i + 1];
+            int ib = final[3*nx*j + 3*i + 2];
             std::cout << ir << " " << ig << " " << ib << "\n";
         }
     }
