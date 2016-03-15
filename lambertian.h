83f3d41077d0d632c496885031637dce4e796f83
andars <afoote97@gmail.com>
2016-03-14 21:57:06 -0600

    remove param name in lambertian to silence warning

--- lambertian.h
+++ lambertian.h
@@ -4,7 +4,7 @@
 class lambertian: public material {
 public:
     lambertian(const vec3& a): albedo(a) {}
-    virtual bool scatter(const ray& r_in, const hit_record& rec, 
+    virtual bool scatter(const ray&, const hit_record& rec,
                          vec3& attentuation, ray& scattered) const {
         vec3 target = rec.p + rec.normal + rand_in_unit_sphere();
         scattered = ray(rec.p, target-rec.p);
