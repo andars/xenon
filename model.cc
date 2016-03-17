82bfef8d741428d485a52f6bf46aa7b98b093769
andars <afoote97@gmail.com>
2016-03-17 15:56:31 -0600

    Take into account stl file's normal, if supplied
    i'm not sure if anyone actually uses the normal in the file,
    but might as well

--- model.cc
+++ model.cc
@@ -4,6 +4,7 @@
 #include "entity.h"
 #include "triangle.h"
 #include "lambertian.h"
+#include "metal.h"
 #include "model.h"
 
 model load_stl(const char* filename) {
@@ -31,12 +32,22 @@ model load_stl(const char* filename) {
 
         uint16_t attr_bytes;
         fread(&attr_bytes, 2, 1, stl);
+        vec3 normal(n[0],n[1],n[2]);
+        vec3 vec0(v0[0],v0[1],v0[2]);
+        vec3 vec1(v1[0],v1[1],v1[2]);
+        vec3 vec2(v2[0],v2[1],v2[2]);
 
-        triangles[i] = new triangle(
-                vec3(v0[0],v0[1],v0[2]),
-                vec3(v1[0],v1[1],v1[2]),
-                vec3(v2[0],v2[1],v2[2]),
-                new lambertian(vec3(0.2,0.2,0.9)));
+        if (normal.length() > 0) {
+            //switch winding order if normal is given & differs from computed
+            vec3 computed = unit_vector(cross(vec1-vec0, vec2-vec0));
+            if ((unit_vector(normal) - computed).length() > 0.01) {
+                vec3 temp = vec1;
+                vec1 = vec2;
+                vec2 = temp;
+            }
+        }
+        triangles[i] = new triangle(vec0, vec1, vec2,
+                                    new metal(vec3(0.988,0.05,0.05),0.1));
     }
     fclose(stl);
 
